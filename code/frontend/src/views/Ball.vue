3b55bc0ce3f4b053b76250e7b4f0e2d874cabf2f
김낙현 <sonata0226@gmail.com>
2020-10-21 18:24:09 +0900

    S03P31B102-89 S03P31B102-90 / Add unity file

---
+++ code/frontend/src/views/Ball.vue
@@ -0,0 +1,20 @@
+!<template>
+  <div class="webgl-content">
+      <div id="ball" style="width: 960px; height: 600px"></div>
+      <div class="footer">
+        <div class="webgl-logo"></div>
+        <div class="fullscreen" onclick="unityInstance.SetFullscreen(1)"></div>
+        <div class="title">Ball</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- code/frontend/src/views/Home.vue
+++ code/frontend/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     Home
+    <router-link to="/Game">Game</router-link>
+    <router-link to="/Ball">Ball</router-link>
   </div>
 </template>
 
