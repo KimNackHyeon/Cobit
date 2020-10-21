24d58a533a4934956c8d5d272f67eca626575780
ParkKeonHo <31854765+ParkKeonHo@users.noreply.github.com>
2020-10-21 18:36:50 +0900

    S03P31B102-91/Add Backend PJT

---
+++ Cobit/src/test/java/com/finalproject/cobit/CobitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.cobit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
