5e0cf2b3111b308c65ad4075e6a47e3f3fa2776e
김낙현 <sonata0226@gmail.com>
2020-10-26 16:42:12 +0900

    S03P31B102-126 / Add StageController API

---
+++ code/Cobit/src/main/java/com/finalproject/cobit/repo/StageRepo.java
@@ -0,0 +1,13 @@
+package com.finalproject.cobit.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.finalproject.cobit.model.Stage;
+
+@Repository
+public interface StageRepo extends JpaRepository<Stage, Long>{
+	List<Stage> findAll();
+}
