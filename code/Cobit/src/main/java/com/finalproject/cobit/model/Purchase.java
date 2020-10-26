ba26da9fb0d74fb455a9c5563095273d46465773
김낙현 <sonata0226@gmail.com>
2020-10-26 14:25:13 +0900

    S03P31B102-123 / Add purchase dto

---
+++ code/Cobit/src/main/java/com/finalproject/cobit/model/Purchase.java
@@ -0,0 +1,54 @@
+package com.finalproject.cobit.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Purchase {
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public Purchase() {
+	}
+
+	public Purchase(User user, Product product) {
+		super();
+		this.user = user;
+		this.product = product;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public String toString() {
+		return "Purchase [user=" + user + ", product=" + product + "]";
+	}
+
+}
