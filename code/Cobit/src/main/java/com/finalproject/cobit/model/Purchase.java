034de12f29fe5bf48e7d2582aa68699118d4a448
ParkKeonHo <31854765+ParkKeonHo@users.noreply.github.com>
2020-10-27 10:11:18 +0900

    Update Spring

--- code/Cobit/src/main/java/com/finalproject/cobit/model/Purchase.java
+++ code/Cobit/src/main/java/com/finalproject/cobit/model/Purchase.java
@@ -8,11 +8,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Purchase {
 
@@ -26,35 +28,5 @@ public class Purchase {
 	@Column
 	private Long productId;
 
-	public Purchase() {
-	}
-
-	public Purchase(Long id, Long userId, Long productId) {
-		super();
-		this.id = id;
-		this.userId = userId;
-		this.productId = productId;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
-
-	public Long getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
-
-	@Override
-	public String toString() {
-		return "Purchase [userId=" + userId + ", productId=" + productId + "]";
-	}
 
 }
