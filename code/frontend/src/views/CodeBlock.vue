6ac7b9aedb8ddbeee1316e80f66420151dd13614
박종준 <poiufgin7373@naver.com>
2020-11-17 15:19:17 +0900

    S03P31B102-258 / Update middle story

--- code/frontend/src/views/CodeBlock.vue
+++ code/frontend/src/views/CodeBlock.vue
@@ -1,13 +1,23 @@
 <template>
   <div class='wrap'>
-    <div class="story" @click="clickStory" v-if="openStory">
-      <div v-if="story[stageNum-1].start_modal!=''" style="width:100%; height:20%; position:absolute; bottom:50%; display:flex; justify-content:center;">
+    <div class="story" @click="clickStory" v-show="openStory && stageType == 1">
+      <div v-show="story[stageNum-1].start_modal!=''" style="width:100%; height:20%; position:absolute; bottom:50%; display:flex; justify-content:center;">
         <div style="width:20%; height:100%; background-color:white; box-shadow: 1px 1px 14px #000000b3; border: 4px solid #ffcf00;color:black;" v-html="story[stageNum-1].start_modal"></div>
       </div>
       <div style="width: 100%; height: 25%; position: absolute; bottom: 25%;">
         <img style="width:auto; height:100%;" src="../assets/images/pen_saying.gif">
       </div>
       <div class="script" v-html="story[stageNum-1].end"></div>
+    </div>
+    <div class="story" @click="clickStory" v-show="openStory2 && stageType == 2">
+      <div v-show="story2[stageNum-1].start_modal!=''" style="width:100%; height:20%; position:absolute; bottom:50%; display:flex; justify-content:center;">
+        <div style="width:20%; height:100%; background-color:white; box-shadow: 1px 1px 14px #000000b3; border: 4px solid #ffcf00;color:black;" v-html="story2[stageNum-1].start_modal"></div>
+      </div>
+      <div style="width: 100%; height: 25%; position: absolute; bottom: 25%;">
+        <img style="width:auto; height:100%;" src="../assets/images/pen_saying.gif">
+      </div>
+      <div v-show="openStory2Start" class="script" v-html="story2[stageNum-1].start"></div>
+      <div v-show="openStory2End" class="script" v-html="story2[stageNum-1].end"></div>
     </div>
     <!-- 튜토리얼 -->
     <div class="tutorial1" v-if="showTutorial == 1 && stageType == 1 && stageNum == 1">
@@ -226,6 +236,32 @@ export default {
           hint:"basicHint5.png"
           },
       ],
+      story2:[
+        {
+          start_modal: "",
+          start:"메일이 왔어요!<br>누구한테서 온거지?<br>한번 열어볼까요?",
+          end:"<h3>낚시와 메일의 합성어인 '피싱메일(Fishing Mail)'</h3><br>피싱메일은 일반메일처럼 보이나 사용자를 속여 바이러스를 퍼트립니다.<br>항상 메일이나 메시지를 열람할때는 주의를 합시다!",
+          hint: ""
+        },
+        {
+          start_modal: "",
+          start:"바이러스는 미리 차단해야해요<br>차단할 수 있는 방법에는 무엇이 있을까요?",
+          end:"<h3>공격을 보호해주는 방화벽!</h3><br>방화벽은 의심스러운 접속을 차단해줘요<br>미리 방화벽을 설정해서 바이러스를 차단해요",
+          hint: ""
+        },
+        {
+          start_modal: "",
+          start:"바이러스가 다시 등장했어요!<br>차단하지 못한 바이러스 미리 예방했나요?",
+          end:"<h3>파일들을 임시 복제하는 백업!</h3><br>파일들을 미리 임시보관소에 저장을 하면<br>바이러스에 걸려도 복원을 할 수 있어요<br>자주 백업하는 습관을 길러 바이러스를 예방합시다",
+          hint: ""
+        },
+        {
+          start_modal: "",
+          start:"이미 감염된 바이러스는 어떻게 하죠?<br>감염된 PC를 치료할 수 있는 방법을 알아봐요",
+          end:"<h3>다양한 솔루션을 가진 백신프로그램</h3><br>백신프로그램은 예방과 치료 그리고 관리까지 바이러스의 종합 해결책이에요<br>자신에게 필요한 백신프로그램을 찾아서 꼭 설치를 해줍시다",
+          hint: ""
+        }
+      ],
         defaultStep:[
         {
           num:0,
@@ -357,6 +393,10 @@ export default {
       starNum: 1,
       stageType: this.$cookies.get('stageInfo').stageType,
       openStory:false,
+      openStory2: true,
+      openStory2Start: false,
+      openStory2End: false,
+      openClear: false,
       buyhint: false,
       hintCount: store.state.kakaoUserInfo.hint,
       showTutorial: 0,
@@ -403,6 +443,8 @@ export default {
       $(".hintBtnbox").css('box-shadow', "unset");
     }
     this.checkBlockArea();
+    this.openStory2 = true;
+    this.openStory2Start = true;
   },
   watch: {
     stageNum() {
@@ -520,7 +562,14 @@ export default {
      },
      clickStory(){
        this.openStory = false;
-       this.onModal();
+       this.openStory2 = false;
+       this.openStory2Start = false;
+       this.openStory2End = false;
+       if(this.stageType == 1) {
+         this.onModal();
+       } else if(this.stageType == 2 && this.openClear) {
+         this.onModal();
+       }
      },
      clickForblock(m,index){
        if(m.num==7 ||m.num==8 ||index==101){
@@ -975,6 +1024,10 @@ export default {
         }, 100);
         this.$router.push('/speech');
       }else{
+        this.openClear = false;
+        this.openStory2 = true;
+        this.openStory2Start = true;
+        this.openStory2End = false;
         this.$refs.myInstance.message('JavascriptHook', 'RestartGame')
         this.LevelLoad();
       }
@@ -987,7 +1040,10 @@ export default {
     },
     handleClear() {
       this.getStar();
+      this.openClear = true;
       this.openStory = true;
+      this.openStory2 = true;
+      this.openStory2End = true;
       this.history.push({move:'clear',move_kor:"스테이지"+this.stageNum+' 성공!',num:-1})
     },
     handleFail() {
@@ -1345,14 +1401,18 @@ export default {
   z-index:1;
 }
 .script{
-      width: 100%;
-    height: 25%;
-    background-color: rgba(0, 0, 0, 0.66);
-    position: absolute;
-    bottom: 0px;
-    color: white;
-    font-size: x-large;
-    padding: 5vh 5vw;
+  width: 100%;
+  height: 25%;
+  background-color: rgba(0, 0, 0, 0.66);
+  position: absolute;
+  bottom: 0px;
+  color: white;
+  font-size: 20px;
+  padding: 20px 5vw;
+}
+
+.script > h3 {
+  margin-bottom: 5px;
 }
 
 .block7{
